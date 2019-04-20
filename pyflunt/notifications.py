09bb4dcd264b48e5c25f0b587ab1ca07badeec10
flaviogf <flavio.fernandes6@gmail.com>
2019-04-20 13:04:59 -0300

    type hint Notifiable

--- pyflunt/notifications.py
+++ pyflunt/notifications.py
@@ -1,4 +1,5 @@
 from abc import ABC
+from typing import Tuple
 
 
 class Notification:
@@ -20,18 +21,19 @@ class Notifiable(ABC):
         self._notifications = []
 
     @property
-    def is_valid(self):
+    def is_valid(self) -> 'bool':
         return not self._notifications
 
     @property
-    def notifications(self):
+    def notifications(self) -> 'Tuple[Notification]':
         return tuple(self._notifications)
 
-    def add_notification(self, notification):
+    def add_notification(self, notification: 'Notification') -> 'None':
         self._notifications.append(notification)
 
-    def add_notifications(self, *notifications):
+    def add_notifications(self, *notifications: 'Notification') -> 'None':
         self._notifications += notifications
 
-    def add_notifications_from_notifiable(self, notifiable):
+    def add_notifications_from_notifiable(self,
+                                          notifiable: 'Notifiable') -> 'None':
         self._notifications += notifiable.notifications
