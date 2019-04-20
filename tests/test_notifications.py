fd5d039ff302def8510f523f64ecb5677fb224da
flaviogf <flavio.fernandes6@gmail.com>
2019-04-20 09:14:46 -0300

    rename tests methods

--- tests/test_notifications.py
+++ tests/test_notifications.py
@@ -4,42 +4,36 @@ from pyflunt.notifications import Notifiable, Notification
 
 
 class NotifiableTests(unittest.TestCase):
-    def test_add_notification_is_valid(self):
-        name = Name()
+    def setUp(self):
+        class Name(Notifiable):
+            pass
 
-        name.add_notification(
+        self._name = Name()
+
+    def test_should_is_valid_false_when_add_notification(self):
+        self._name.add_notification(
             Notification(field='first_name', message='first name invalid'))
 
-        self.assertFalse(name.is_valid)
+        self.assertFalse(self._name.is_valid)
 
-    def test_add_notification_len_notifications(self):
-        name = Name()
-
-        name.add_notification(
+    def test_should_has_a_notification_when_add_notification(self):
+        self._name.add_notification(
             Notification(field='first_name', message='first name invalid'))
 
-        self.assertEqual(1, len(name.notifications))
+        self.assertEqual(1, len(self._name.notifications))
 
-    def test_add_notifications_is_valid(self):
-        name = Name()
-
-        name.add_notifications(
+    def test_should_is_valid_false_when_add_notifications(self):
+        self._name.add_notifications(
             Notification(field='first name', message='first name invalid'),
             Notification(field='last name', message='last name invalid'),
         )
 
-        self.assertFalse(name.is_valid)
+        self.assertFalse(self._name.is_valid)
 
-    def test_add_notifications_len_notifications(self):
-        name = Name()
-
-        name.add_notifications(
+    def test_should_has_two_notifications_when_add_notifications(self):
+        self._name.add_notifications(
             Notification(field='first name', message='first name invalid'),
             Notification(field='last name', message='last name invalid'),
         )
 
-        self.assertEqual(2, len(name.notifications))
-
-
-class Name(Notifiable):
-    pass
+        self.assertEqual(2, len(self._name.notifications))
