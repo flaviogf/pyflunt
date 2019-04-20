d73daf514314caf7eb40917a91162ac2e34f06fd
flaviogf <flavio.fernandes6@gmail.com>
2019-04-20 09:37:55 -0300

    implementation add_notifications_from_notifiable

--- tests/test_notifications.py
+++ tests/test_notifications.py
@@ -3,11 +3,16 @@ import unittest
 from pyflunt.notifications import Notifiable, Notification
 
 
+class Name(Notifiable):
+    pass
+
+
+class Customer(Notifiable):
+    pass
+
+
 class NotifiableTests(unittest.TestCase):
     def setUp(self):
-        class Name(Notifiable):
-            pass
-
         self._name = Name()
 
     def test_should_is_valid_false_when_add_notification(self):
@@ -37,3 +42,25 @@ class NotifiableTests(unittest.TestCase):
         )
 
         self.assertEqual(2, len(self._name.notifications))
+
+    def test_should_is_valid_false_when_add_notifications_from_notifiable(
+            self):
+        self._name.add_notification(
+            Notification(field='first_name', message='first name invalid'))
+
+        steve = Customer()
+
+        steve.add_notifications_from_notifiable(notifiable=self._name)
+
+        self.assertFalse(steve.is_valid)
+
+    def test_should_has_a_notification_when_add_notifications_from_notifiable(
+            self):
+        self._name.add_notification(
+            Notification(field='first_name', message='first name invalid'))
+
+        steve = Customer()
+
+        steve.add_notifications_from_notifiable(notifiable=self._name)
+
+        self.assertEqual(1, len(steve.notifications))
