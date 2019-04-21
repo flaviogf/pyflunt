de80f0aa83f8337ee1d2f596ed5afb0891527076
flaviogf <flavio.fernandes6@gmail.com>
2019-04-21 08:51:34 -0300

    setup and tests

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='pyflunt',
+    version='0.0.1',
+    description='Python implementation of Domain Notification Pattern based in Flunt(.NET) developed by @andrebaltieri',
+    author='Flavio Fernandes',
+    author_email='flavio.fernandes6@gmail.com',
+    url='https://github.com/flaviogf/pyflunt/',
+    packages=('pyflunt',),
+)
